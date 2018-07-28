8740ecd018562ff53ed67a63519da03ed12a654f
Michal Gebarowski <vmaaik@gmail.com>
2018-07-28 16:19:17 +0100

    intro

---
+++ kickoff5-calculated-propesties.ps1
@@ -0,0 +1,18 @@
+﻿# Calculated Properties
+
+#intro
+
+Get-Process | more
+Get-Process | Select-Object -Property Name, VM, PM -First 10
+Get-Process | Select-Object -Propert Name, @{name= "MEMO"; e={ '{0:N2}' -f ($PSItem.VM / 1MB)}}, PM -First 10
+Get-Process | Select-Object -Propert Name, @{name= "MEMO"; e={$PSItem.VM/1MB}}, PM -First 10
+
+Get-Content -Path .\utils\Cities.txt | Select-Object @{name='Lenght' ; e={$PSItem.GetType()}}
+$city = New-Object -TypeName psobject
+$cities = Get-Content -Path .\utils\Cities.txt 
+foreach ($city in $cities){
+$city | Add-Member -MemberType NoteProperty -Name CityName -Value @{name= "MEMO"; e={$PSItem.BaseObject}}
+}
+
+Get-Service | Select-Object -Property @{Name = 'Name'; Expression = {$_.Name}}
+Get-ChildItem .\utils\Cities.txt | Select-Object -Property Name, @{label = 'FileSize'; expression = { $_.Length/1KB }}
